6cf5e503c5627de40b84d8d75b1d79b8560c1688
Alex <pasechnyy@proitr.ru>
2021-11-25 22:16:55 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <input v-model="text" />
+    <button @click="onClick">Расшифровать</button>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      text: "wwaldaadicffenn",
+      message: "",
+    };
+  },
+  methods: {
+    decrypt(str) {
+      return str.replace(/(.)\1(?<!\1{3})(?!\1)/g, "");
+    },
+    onClick() {
+      this.message = this.text;
+      while (/(.)\1(?<!\1{3})(?!\1)/g.test(this.message)) {
+        this.message = this.decrypt(this.message);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  margin: auto;
+}
+</style>
